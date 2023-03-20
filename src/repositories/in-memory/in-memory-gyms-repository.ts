d207063a594cd31138d216f2de8819b2e79c6e0f
Matheus <etec.matheus@gmail.com>
2023-03-20 19:08:42 -0300

    refactor(gym): improve create function

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,10 +1,20 @@
-import { Gym } from "@prisma/client";
+import { Gym, Prisma } from "@prisma/client";
+import { randomUUID } from "node:crypto";
 import { GymsRepository } from "../gyms-repository";
 
 export class InMemoryGymsRepository implements GymsRepository {
   private gyms: Gym[] = [];
 
-  async create(gym: Gym): Promise<Gym> {
+  async create(data: Prisma.GymCreateInput): Promise<Gym> {
+    const gym = {
+      id: data.id ?? randomUUID(),
+      title: data.title,
+      description: data.description ?? null,
+      phone: data.phone ?? null,
+      latitude: new Prisma.Decimal(data.latitude.toString()),
+      longitude: new Prisma.Decimal(data.longitude.toString()),
+    };
+
     this.gyms.push(gym);
 
     return gym;
@@ -12,7 +22,6 @@ export class InMemoryGymsRepository implements GymsRepository {
 
   async findById(id: string): Promise<Gym | null> {
     const gym = this.gyms.find((gym) => gym.id === id);
-
     if (!gym) return null;
 
     return gym;
